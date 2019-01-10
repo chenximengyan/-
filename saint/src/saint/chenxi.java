7e06bb080a865df7115ca7f7e93b4dcded8ca0c1
user <user@192.168.3.19>
2019-01-10 11:25:10 +0900

    wewqeqweasdwqeasd qwerqwerqw

---
+++ saint/src/saint/chenxi.java
@@ -0,0 +1,28 @@
+package saint;
+
+
+	import java.text.DecimalFormat;
+import java.util.Scanner;
+	public class chenxi {
+	    public static void main(String args[]) {
+	        Scanner input = new Scanner(System.in);
+	        DecimalFormat df = new DecimalFormat("0.0");//使用decimalformat精确小数位数
+	        while(input.hasNext()) {
+	            double x,y;
+	            x = input.nextDouble();
+	            if(x > 0) {
+	                y = x*x+1;
+	            }
+	            else if(x < 0) {
+	                y = -x;
+	            }
+	            else {
+	                y = 100;
+	            }
+	            System.out.println(df.format(y));
+	        }
+	        input.close();
+	    }
+
+	}
+	
